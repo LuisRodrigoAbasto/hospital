a9eedc805ce9c4bf0cbfec4e62e3f09af9447769
Luis Rodrig Abasto <vida12345abasto@gmail.com>
2019-10-05 19:16:02 -0400

    terminado Carpeta 4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,19 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+// import { PagesComponent } from './pages/pages.component';
 
 const appRoutes: Routes = [
-    { 
-        path: '', 
-        component: PagesComponent,
-        children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-        ]
-    },
+    // { 
+    //     path: '', 
+    //     component: PagesComponent,
+    //     children: [
+    //         { path: 'dashboard', component: DashboardComponent },
+    //         { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    //     ]
+    // },
 
     { path: 'login', component: LoginComponent },
     { path: 'register', component: LoginComponent },
